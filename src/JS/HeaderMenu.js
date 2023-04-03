5d60b74294b3d8ef2916a9b67dd5aa50cb912e4c
heardofreyy <abderrahmenebendoukha8@gmail.com>
2023-04-03 12:48:55 +0100

    remove on scroll

--- src/JS/HeaderMenu.js
+++ src/JS/HeaderMenu.js
@@ -1,7 +1,11 @@
 const navBarMenu = document.getElementById('nav-bar-menu')
 
 
-
+document.onclick= (e) => {
+    if(e.target.id !== 'nav-bar-menu' && e.target.id !== 'menu-icon') {
+        navBarMenu.classList.remove('openNav')
+    }
+}
 
 
 const openNavMenu = () => {
